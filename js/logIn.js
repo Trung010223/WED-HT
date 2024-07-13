da02d102e28927737f85107190a0e7893f29862b
Trung <ngoquangtrungnqt@gmail.com>
2024-07-13 13:36:22 +0700

    sua

--- js/logIn.js
+++ js/logIn.js
@@ -1,20 +1,4 @@
 document.addEventListener("DOMContentLoaded", function () {
-  const eyeButton = document.getElementById("eye");
-  const passwordInput = eyeButton.previousElementSibling;
-
-  eyeButton.addEventListener("click", function () {
-    this.classList.toggle("open");
-    this.children[0].classList.toggle("fa-eye-slash");
-    this.children[0].classList.toggle("fa-eye");
-
-    if (this.classList.contains("open")) {
-      passwordInput.type = "text";
-    } else {
-      passwordInput.type = "password";
-    }
-  });
-});
-document.addEventListener("DOMContentLoaded", function () {
   const usernameInput = document.getElementById("username");
   const passwordInput = document.getElementById("password");
   const loginForm = document.getElementById("form-login");
